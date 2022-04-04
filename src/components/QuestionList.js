99bf565b6437aecb86db11605cf7329914b810c1
NicMortelliti <npmortelliti@gmail.com>
2022-04-04 16:16:16 -0700

    Add delete question functionality

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,12 +2,23 @@ import React from "react";
 import QuestionItem from "./QuestionItem";
 
 function QuestionList({ questions, setQuestions }) {
+  function handleDeleteItem(deletedQuestion) {
+    const updatedQuestions = questions.filter(
+      question => question.id !== deletedQuestion.id
+    );
+    setQuestions(updatedQuestions);
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>
         {questions.map(question => (
-          <QuestionItem key={question.id} question={question} />
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </section>
