a9dbf52f7faf4a68ff20c51a32d68332537da31f
NicMortelliti <npmortelliti@gmail.com>
2022-04-04 16:06:36 -0700

    Add ability to add new questions to the database.

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,15 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  const [questions, setQuestions] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:4000/questions")
-      .then(r => r.json())
-      .then(data => setQuestions(data));
-  }, []);
-
+function QuestionList({ questions, setQuestions }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
