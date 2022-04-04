5944253006eb6bbdfba641a795df74df7ac4f33e
NicMortelliti <npmortelliti@gmail.com>
2022-04-04 16:42:02 -0700

    Add some functionality for patching questions. Not yet completed.

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question, onDeleteItem }) {
+function QuestionItem({ question, setQuestions, onDeleteItem }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -17,13 +17,26 @@ function QuestionItem({ question, onDeleteItem }) {
       .then(() => onDeleteItem(question));
   }
 
+  function handleChange(event) {
+    const newCorrectAnswerID = event.target.value;
+    fetch(`http://localhost:4000/questions/${newCorrectAnswerID}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newCorrectAnswerID),
+    })
+      .then(r => r.json())
+      .then(() => setQuestions());
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>
+          {options}
+        </select>
       </label>
       <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
